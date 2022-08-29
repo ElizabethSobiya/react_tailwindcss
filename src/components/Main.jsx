911266c0cb3645e4678a1fb197efdcab2ce65a6c
sobiya <sarockia@asu.edu>
2022-08-29 22:55:07 +0530

    navbar

---
+++ src/components/Main.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Main() {
+  return (
+    <>
+    <div className='text-white '>
+            <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center  '>
+             <p className='uppercase text-[#00df9a] font-bold p-2'>growing with data analytics</p>
+             <h1 className='md:text-7xl  text-4xl font-bold sm:text-6xl'>Grow with Data.</h1>
+             <div>
+                <p className='md:text-5xl sm:text-4xl text-xl font-bold '>Fast, flexible financing for </p>
+             </div>
+            </div>
+    </div>
+    </>
+  )
+}
+
+export default Main
