c3ac58b5ef2412a2968cacf48635c8ab69725285
sobiya <sarockia@asu.edu>
2022-08-30 12:33:50 +0530

    all files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import Navbar from './components/Navbar';
 import Main from './components/Main'
+import Analytics from './components/Analytics';
+import Newsletter from './components/Newsletter';
+import Cards from './components/Cards';
+import Footer from './components/Footer'
 
 function App() {
   return (
     <>
     <Navbar/>
     <Main/>
+    <Analytics/>
+    <Newsletter/>
+    <Cards/>
+    <Footer/>
     </>
   );
 }
